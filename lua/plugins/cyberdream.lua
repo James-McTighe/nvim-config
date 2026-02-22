304e18bdc873dfc8038ff27ba1cd74c45103abcb
James McTighe <james.mctighe97@gmail.com>
2026-02-22 18:31:33 -0500

    updated background color

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -3,11 +3,15 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-        transparent = true,    -- Enable transparent background
+        transparent = false,    -- Enable transparent background
         italic_comments = true, -- Enable italic comments
         hide_fillchars = true,  -- Hide ~ at the end of the buffer
         borderless_pickers = false, -- Muted borders for Telescope/Fzf-lua
         terminal_colors = true, -- Use theme colors in terminal
+        saturation = 0.9,
+        colors = {
+          bg = "#222135"
+        }
     },
     config = function(_, opts)
         require("cyberdream").setup(opts)
