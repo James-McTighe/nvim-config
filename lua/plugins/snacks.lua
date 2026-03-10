20e429c0d0751fcc9dbefb221db963cd67b62449
James McTighe <james.mctighe@lanelink.com>
2026-03-10 11:20:38 -0400

    configured snacks so all windows except the explorer are full screen

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      layout = {
+        fullscreen = true
+      },
+      sources = {
+        -- Override for teh explorer specifically
+        explorer = {
+          layout = {
+            preset = "sidebar",
+            fullscreen = false,
+          }
+        }
+      }
+    }
+  }
+}
