b02530b30c72879def84d9c8e5a427f41e1fdaf2
James McTighe <james.mctighe@lanelink.com>
2026-03-27 09:11:05 -0400

    pomodoro doesn't start on startup

--- lua/plugins/pomodoro.lua
+++ lua/plugins/pomodoro.lua
@@ -19,7 +19,7 @@ return {
     }
   },
   opts = {
-    start_at_launch = true,
+    start_at_launch = false,
     work_duration = 25,
     break_duration = 5,
     delay_duration = 1, -- The additionnal work time you get when you delay a break
