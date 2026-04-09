d6c07801012127f5eb09790bf1fa6bf5d4d1bad9
James McTighe <james.mctighe97@gmail.com>
2026-04-09 02:34:51 -0400

    added tiny glimmer and updated snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,22 @@
 return {
   "folke/snacks.nvim",
+  event = "VeryLazy",
+  priority = 11,
   opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 4000,
+    },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = false },
     picker = {
       layout = {
         fullscreen = true
