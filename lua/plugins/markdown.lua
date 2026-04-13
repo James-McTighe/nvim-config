cfbd7c258391990ae6002fa6cf25c773337b24c1
James McTighe <james.mctighe@lanelink.com>
2026-04-13 12:33:10 -0400

    updated markdown key binds and homescreen

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,5 +2,14 @@ return {
   "iamcco/markdown-preview.nvim",
   init = function()
     vim.g.mkdp_auto_close = 0
-  end
+  end,
+  ft = { "markdown" },
+  keys = {
+    {
+      "<leader>cp",
+      "<Cmd>MarkdownPreview<CR>",
+      desc = "Open markdown-preview"
+    }
+  }
+
 }
