4869485b3777d45fd88e0ea01daf498e67d26a8a
James McTighe <james.mctighe@lanelink.com>
2026-04-23 13:02:37 -0400

    updated keybinds for diffview

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,17 @@
 -- In lua/config/keymaps.lua
 -- Force delete the existing mapping if it's already set
 vim.keymap.del("n", "<leader>gd")
+vim.keymap.del("n", "<leader>gD")
 
 -- Then set your new one
 vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
+vim.keymap.set("n", "<leader>gD",
+        function ()
+          vim.ui.input({prompt = "Compare with branch or commit hash: "}, function(input)
+            if input then
+              vim.cmd("DiffviewOpen " .. input)
+            end
+          end)
+        end,
+  {desc = "DiffView a branch or commit hash"}
+)
