5a8a5213bfa9bcb76c60d94e1d741cdf0024b8b3
James McTighe <james.mctighe97@gmail.com>
2026-05-26 16:49:35 -0400

    updated lualine to show python env

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,28 @@
+local function python_env()
+    -- Check for standard venv or virtualenv
+    local venv = os.getenv("VIRTUAL_ENV")
+    if venv then
+        return string.match(venv, "[^/]+$")
+    end
+
+    -- Check for Conda environment
+    local conda = os.getenv("CONDA_DEFAULT_ENV")
+    if conda then
+        return conda
+    end
+
+    return ""
+end
+
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = {
-    sections = {
-      lualine_z = {
-        { "datetime", style = "%I:%M %p" }, -- %I is 12hr, %p is AM/PM
-      },
+    "nvim-lualine/lualine.nvim",
+    opts = {
+        sections = {
+            lualine_c = { "branch", "diff", "diagnostics" },
+            lualine_x = { python_env },
+            lualine_z = {
+                { "datetime", style = "%I:%M %p" }, -- %I is 12hr, %p is AM/PM
+            },
+        },
     },
-  },
 }
