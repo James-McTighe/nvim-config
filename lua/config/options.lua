5e4fe402e41840b765af2d147bfffeb83ba941b3
James McTighe <james.mctighe97@gmail.com>
2026-09-12 03:21:38 -0400

    updated terminal colors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,27 @@
 -- Add any additional options here
 vim.g.autoformat = false
 vim.opt.hlsearch = false
+
+-- Add to lua/config/options.lua or lua/plugins/ui.lua
+local colors = {
+  "#1e1e2e", -- 0: Black
+  "#f38ba8", -- 1: Red
+  "#a6e3a1", -- 2: Green
+  "#f9e2af", -- 3: Yellow
+  "#89b4fa", -- 4: Blue (bright enough to read)
+  "#f5c2e7", -- 5: Magenta
+  "#94e2d5", -- 6: Cyan
+  "#cdd6f4", -- 7: White
+  "#585b70", -- 8: Bright Black
+  "#f38ba8", -- 9: Bright Red
+  "#a6e3a1", -- 10: Bright Green
+  "#f9e2af", -- 11: Bright Yellow
+  "#b4befe", -- 12: Bright Blue
+  "#f5c2e7", -- 13: Bright Magenta
+  "#94e2d5", -- 14: Bright Cyan
+  "#a6adc8", -- 15: Bright White
+}
+
+for i, col in ipairs(colors) do
+  vim.g["terminal_color_" .. (i - 1)] = col
+end
