6f89a0ce9dd39e49040319e1a50ed204a69f2cc2
James McTighe <james.mctighe97@gmail.com>
2026-09-13 03:49:40 -0400

    modified lualine
    might change this later

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    opts.options.component_separators = ""
-    opts.options.section_separators = { left = "", right = "" }
-    
-    opts.sections.lualine_a = { { "mode", separator = { left = "", right = "" } } }
-    opts.sections.lualine_z = { { "location", separator = { left = "", right = "" } } }
-  end,
+    "nvim-lualine/lualine.nvim",
+    opts = {
+        options = {
+            theme = "auto",
+            section_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
+            globalstatus = true,
+        },
+        sections = {
+            lualine_a = { { "branch", icon = "" }, "mode" },
+            lualine_b = {
+                { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
+            },
+            lualine_c = { "selectioncount" },
+
+            lualine_x = { "diagnostics", "diff" },
+            lualine_y = { "progress" },
+            lualine_z = { "lsp_status" },
+        },
+    },
 }
