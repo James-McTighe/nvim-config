60af86b2b5f15d6bc61389bb839a1afe463ff1ba
James McTighe <james.mctighe97@gmail.com>
2026-09-13 16:08:42 -0400

    added h and l mappings to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -49,6 +49,17 @@ return {
         window = {
             position = "left",
             width = 40,
+            mappings = {
+                ["h"] = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == "directory" and node:is_expanded() then
+                        require("neo-tree.sources.filesystem").toggle_directory(state, node)
+                    else
+                        require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+                    end
+                end,
+                ["l"] = "open",
+            },
         },
         default_component_configs = {
             indent = {
