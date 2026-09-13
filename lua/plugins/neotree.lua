d80d6d5e8850aa4ecc9dc113a96e08ccd538c867
James McTighe <james.mctighe97@gmail.com>
2026-09-13 02:50:47 -0400

    added neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,40 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        window = {
+            position = "left",
+            width = 40,
+        },
+        default_component_configs = {
+            indent = {
+                indent_size = 2,
+                padding = 1,
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+                with_expanders = true,
+                expander_collapsed = "",
+                expander_expanded = "",
+            },
+            --   -- icon = {
+            --   --   folder_closed = "󰉋",
+            --   --   folder_open = "󰝰",
+            --   --   folder_empty = "󰉖",
+            --   -- },
+            --   git_status = {
+            --     symbols = {
+            --       added = "A",
+            --       modified = "M",
+            --       deleted = "D",
+            --       renamed = "R",
+            --       untracked = "",
+            --       ignored = "i",
+            --       unstaged = "U",
+            --       staged = "S",
+            --       conflict = "X",
+            --     },
+            --   },
+        },
+    },
+}
