0e06dbf97b6d2e1c7ca261b9af0f9f2ba47d147d
James McTighe <james.mctighe97@gmail.com>
2026-09-13 15:59:58 -0400

    updated autosave notification to include file name

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -2,19 +2,19 @@ return {
   "Pocco81/auto-save.nvim",
   config = function()
     require("auto-save").setup({
-      -- Your configuration options here
-      -- e.g., trigger on specific events, set a debounce delay
-      debounce_delay = 135, -- milliseconds
-      trigger_events = {"InsertLeave", "TextChanged"}, -- events that trigger the save
-      -- add a message when saved
+      debounce_delay = 135,
+      trigger_events = { "InsertLeave", "TextChanged" },
       execution_message = {
-        message = function() -- message to print on save
-          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        message = function()
+          local filename = vim.fn.expand("%:t")
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          return string.format("AutoSave: saved %s at %s", filename, vim.fn.strftime("%H:%M:%S"))
         end,
-        dim = 0.18, -- dim the color of `message`
-        cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`
+        dim = 0.18,
+        cleaning_interval = 1250,
       },
     })
   end,
 }
-
