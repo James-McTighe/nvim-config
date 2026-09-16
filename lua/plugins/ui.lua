025210ce7fa36234c4c8ca0028886a5121ec03c8
James McTighe <james.mctighe97@gmail.com>
2026-09-16 19:54:34 -0400

    fixed bug after updating nvim
    border colors are still fucked

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,25 +1,49 @@
 return {
-    -- Modern rounded borders for LSP floating windows
+    -- 1. LSP Floating Hover & Signature Help Rounded Borders
     {
         "neovim/nvim-lspconfig",
-        opts = function()
-            local ui = require("lspconfig.ui.windows")
-            ui.default_options.border = "rounded"
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-            vim.lsp.handlers["textDocument/signatureHelp"] =
-                vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+        opts = {
+            diagnostics = {
+                float = { border = "rounded" },
+            },
+        },
+        init = function()
+            local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+            function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+                opts = opts or {}
+                opts.border = opts.border or "rounded"
+                return orig_util_open_floating_preview(contents, syntax, opts, ...)
+            end
         end,
     },
+
+    -- 2. Floating rounded borders for snacks.nvim (pickers, notifications, inputs, terminals)
+    {
+        "folke/snacks.nvim",
+        opts = {
+            styles = {
+                float = {
+                    border = "rounded",
+                },
+                notification = {
+                    border = "rounded",
+                },
+                picker = {
+                    border = "rounded",
+                },
+            },
+        },
+    },
+
+    -- 3. Restore visible border highlight colors
     {
         "LazyVim/LazyVim",
         opts = function()
-            -- Subtle background for float elements with soft borders
-            vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#585b70", bg = "none" })
-            vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#45475a", fg = "none", bold = true })
-            vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#585b70" })
-            vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#585b70", bg = "none" })
-            vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { fg = "#89b4fa", bg = "none" })
-            vim.api.nvim_set_hl(0, "SnacksPickerMatch", { fg = "#89b4fa", bold = true })
+            -- Distinct lavender/light gray border so floating boxes stand out
+            vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89b4fa", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+            vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#89b4fa", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { fg = "#89b4fa", bg = "NONE" })
         end,
     },
 }
